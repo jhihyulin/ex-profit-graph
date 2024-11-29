644f1486f55804291e5280ae76a7d4fd3f09aef6
jhih_yu <74854953+jhihyulin@users.noreply.github.com>
2024-11-29 15:19:27 +0800

    fix: key missing

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -18,9 +18,11 @@ export default function Generate() {
     <div className="flex items-center justify-center min-h-screen min-w-screen p-4 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-wrap gap-8 items-center justify-center w-full">
         <div className="flex flex-col gap-4 justify-center bg-gray-600 p-4 justify-center rounded-md w-full lg:w-3/5 max-w-sm aspect-square">
-          {formTypeSelections.map((selection) =>
-            formType === selection.value ? selection.generate : null
-          )}
+          {formTypeSelections.map((selection) => (
+            <div key={selection.value}>
+              {formType === selection.value ? selection.generate : null}
+            </div>
+          ))}
         </div>
         <div className="flex flex-col gap-4 justify-left bg-gray-600 p-4 justify-center rounded-md w-full lg:w-2/5 max-w-sm">
           <select
@@ -34,11 +36,15 @@ export default function Generate() {
             }}
           >
             {formTypeSelections.map((selection) => (
-              <option value={selection.value}>{selection.name}</option>
+              <option key={selection.value} value={selection.value}>
+                {selection.name}
+              </option>
             ))}
           </select>
           {formTypeSelections.map((selection) =>
-            formType === selection.value ? selection.form : null
+            formType === selection.value ? (
+              <div key={selection.value}>{selection.form}</div>
+            ) : null
           )}
         </div>
       </main>
